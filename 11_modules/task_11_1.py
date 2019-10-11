ea5ac38edc7fd555957d7ff62b75d9115638844a
Ilyas <kareeem@yandex.ru>
2019-10-11 15:39:26 +0300

    Выполнил задания 11

---
+++ 11_modules/task_11_1.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+'''
+Задание 11.1
+
+Создать функцию parse_cdp_neighbors, которая обрабатывает
+вывод команды show cdp neighbors.
+
+У функции должен быть один параметр command_output, который ожидает как аргумент вывод команды одной строкой (не имя файла). Для этого надо считать все содержимое файла в строку.
+
+Функция должна возвращать словарь, который описывает соединения между устройствами.
+
+Например, если как аргумент был передан такой вывод:
+R4>show cdp neighbors
+
+Device ID    Local Intrfce   Holdtme     Capability       Platform    Port ID
+R5           Fa 0/1          122           R S I           2811       Fa 0/1
+R6           Fa 0/2          143           R S I           2811       Fa 0/0
+
+Функция должна вернуть такой словарь:
+
+    {('R4', 'Fa0/1'): ('R5', 'Fa0/1'),
+     ('R4', 'Fa0/2'): ('R6', 'Fa0/0')}
+
+В словаре интерфейсы должны быть записаны без пробела между типом и именем. То есть так Fa0/0, а не так Fa 0/0.
+
+Проверить работу функции на содержимом файла sh_cdp_n_sw1.txt
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+'''
+
+def parse_cdp_neighbors(command_output):
+    cdp = {}
+    for line in command_output.split('\n'):
+        if line.count('>'):
+            local_dev, *rest = line.split('>')
+        elif line.count('Eth'):
+            dev, _, local_intf, *rest, port = line.split()
+            key = tuple([local_dev, f'Eth{local_intf}'])
+            cdp[key] = tuple([dev, f'Eth{port}'])
+    return cdp
+
+#f = open('sh_cdp_n_sw1.txt')
+#print(parse_cdp_neighbors(f.read()))
+#f.close()
---
+++ 11_modules/task_11_2.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+'''
+Задание 11.2
+
+Создать функцию create_network_map, которая обрабатывает
+вывод команды show cdp neighbors из нескольких файлов и объединяет его в одну общую топологию.
+
+У функции должен быть один параметр filenames, который ожидает как аргумент список с именами файлов, в которых находится вывод команды show cdp neighbors.
+
+Функция должна возвращать словарь, который описывает соединения между устройствами.
+Структура словаря такая же, как в задании 11.1:
+    {('R4', 'Fa0/1'): ('R5', 'Fa0/1'),
+     ('R4', 'Fa0/2'): ('R6', 'Fa0/0')}
+
+
+Cгенерировать топологию, которая соответствует выводу из файлов:
+* sh_cdp_n_sw1.txt
+* sh_cdp_n_r1.txt
+* sh_cdp_n_r2.txt
+* sh_cdp_n_r3.txt
+
+В словаре, который возвращает функция create_network_map, не должно быть дублей.
+
+С помощью функции draw_topology из файла draw_network_graph.py нарисовать схему на основании топологии, полученной с помощью функции create_network_map.
+Результат должен выглядеть так же, как схема в файле task_11_2a_topology.svg
+
+
+При этом:
+* Расположение устройств на схеме может быть другим
+* Соединения должны соответствовать схеме
+
+Не копировать код функций parse_cdp_neighbors и draw_topology.
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+
+> Для выполнения этого задания, должен быть установлен graphviz:
+> apt-get install graphviz
+
+> И модуль python для работы с graphviz:
+> pip install graphviz
+
+'''
+from draw_network_graph import draw_topology
+
+def create_network_map(filenames):
+    cdp = {}
+    for filename in filenames:
+        with open(filename) as f:
+            for line in f:
+                if line.count('>'):
+                    local_dev, *rest = line.split('>')
+                elif line.count('Eth'):
+                    dev, _, local_intf, *rest, port = line.split()
+                    key = tuple([local_dev, f'Eth{local_intf}'])
+                    value = tuple([dev, f'Eth{port}'])
+                    if not cdp.get(value):
+                        cdp[key] = value
+    return cdp
+draw_topology(create_network_map(['sh_cdp_n_sw1.txt', 'sh_cdp_n_r1.txt', 'sh_cdp_n_r2.txt', 'sh_cdp_n_r3.txt']))
+#print(create_network_map(['sh_cdp_n_sw1.txt', 'sh_cdp_n_r1.txt', 'sh_cdp_n_r2.txt', 'sh_cdp_n_r3.txt']))
